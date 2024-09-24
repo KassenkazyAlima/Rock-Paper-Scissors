c5b0fcda814529d1f5da5e9c95730f24776c7c32
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-09-24 17:50:24 +0300

    first new method

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+console.log("Hello World!");
